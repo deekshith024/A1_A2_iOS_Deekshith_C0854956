5130d4e79e07f52ea1733fb3ed77c1aa8be66cd5
deekshith024 <deekshithreddy0001@gmail.com>
2022-05-24 18:16:14 -0400

    Initial Commit

---
+++ A1_A2_iOS_Deekshith_C0854956/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A1_A2_iOS_Deekshith_C0854956
+//
+//  Created by Karthik Reddy Rondla on 2022-05-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
